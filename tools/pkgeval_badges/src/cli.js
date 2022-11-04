b8d34276c980556719aca18a947e3ce6396d9b80
Tim Besard <tim.besard@gmail.com>
2022-11-04 15:38:10 +0100

    Fix badge generation.

--- tools/pkgeval_badges/src/cli.js
+++ tools/pkgeval_badges/src/cli.js
@@ -2,24 +2,13 @@
 
 const fs = require('fs');
 const path = require('path');
-const { BadgeFactory } = require('gh-badges')
+const { makeBadge, ValidationError } = require('badge-maker');
 
 // find the most recent database with pkgeval results
 const input_dir = path.resolve(__dirname, "../../../pkgeval/by_date/latest/")
 const latest_date = fs.readlinkSync(input_dir)
 const input = path.resolve(input_dir, "db.json")
 const db = require(input)
-
-// reconstruct the version number
-const first_test = Object.values(db.tests)[0]
-const julia = first_test.julia
-version = `${julia.major}.${julia.minor}.${julia.patch}`
-if (julia.prerelease.length > 0) {
-    version = version.concat("-", julia.prerelease.join('.'))
-}
-if (julia.build.length > 0) {
-    version = version.concat("+", julia.prerelease.join('.'))
-}
 
 // helper to generate redirecting HTML
 function redirect(url) {
@@ -41,7 +30,6 @@ function redirect(url) {
 output_dir = path.resolve(__dirname, "../../../public/pkgeval_badges")
 fs.existsSync(output_dir) || fs.mkdirSync(output_dir, { recursive: true })
 
-const bf = new BadgeFactory()
 const formats = {
     'ok':   ['passing', 'brightgreen'],
     'skip': ['skipped', 'gray'],
@@ -50,12 +38,12 @@ const formats = {
 }
 
 // generate badges for all packages
-for (var uuid in db.tests) {
-    test = db.tests[uuid]
+for (var package in db.tests) {
+    test = db.tests[package]
 
     // put the badges in directories grouped by the first letter of its name,
     // or GitHub prevents viewing the entire listing
-    key = test.name[0].toUpperCase()
+    key = package[0].toUpperCase()
     badge_dir = path.join(output_dir, key)
     fs.existsSync(badge_dir) || fs.mkdirSync(badge_dir)
 
@@ -64,25 +52,27 @@ for (var uuid in db.tests) {
 
     // generate the badges
     format_main_badge = {
-        text: ['PkgEval', text],
+        label: 'PkgEval',
+        message: text,
         color: color,
-        template: 'flat',
+        style: 'flat',
     }
     format_named_badge = {
-        text: [test.name, text],
+        label: package,
+        message: text,
         color: color,
-        template: 'flat',
+        style: 'flat',
     }
 
-    svg_main_badge = bf.create(format_main_badge)
-    svg_named_badge = bf.create(format_named_badge)
+    svg_main_badge = makeBadge(format_main_badge)
+    svg_named_badge = makeBadge(format_named_badge)
 
-    fs.writeFileSync(path.join(badge_dir, test.name + ".svg"), svg_main_badge)
-    fs.writeFileSync(path.join(badge_dir, test.name + ".named.svg"), svg_named_badge)
+    fs.writeFileSync(path.join(badge_dir, package + ".svg"), svg_main_badge)
+    fs.writeFileSync(path.join(badge_dir, package + ".named.svg"), svg_named_badge)
 
     // generate a redirect to the log
-    fs.writeFileSync(path.join(badge_dir, test.name + ".html"),
-                     redirect(`https://s3.amazonaws.com/julialang-reports/nanosoldier/pkgeval/by_date/${latest_date}/${test.name}.${version}.log`))
+    fs.writeFileSync(path.join(badge_dir, package + ".html"),
+                     redirect(`https://s3.amazonaws.com/julialang-reports/nanosoldier/pkgeval/by_date/${latest_date}/${package}.primary.log`))
 }
 
 // generate a redirect to the report
